f669090edaa39b96a61c9fa13eef95aea4d5dfaa
bharvey <bart.harvey@utoronto.ca>
2024-05-16 19:06:33 -0400

    I starts at 2

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,7 +4,7 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 1;
+        int i = 2;
         while (i < 100) {
 
             // Find out which numbers divide i.
