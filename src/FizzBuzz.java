92421552a793923e114f875f802ded7cb6aaec1d
bharvey <bart.harvey@utoronto.ca>
2024-05-16 19:34:25 -0400

    Extracted Fizz/Buzz number check

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,29 +7,34 @@ class FizzBuzz {
         int i = 2;
         while (i < 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-        i = i + 1;
+            i = getI(i);
         }
+    }
+
+    public static int getI(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i = i + 1;
+        return i;
     }
 }
